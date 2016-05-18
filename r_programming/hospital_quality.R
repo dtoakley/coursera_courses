e3e72df513d0e01d47c31c3cf685fc842d5305fe
Dan Lewis-Toakley <dan@avaaz.org>
2016-05-19 00:13:53 +0100

    initial complete

--- r_programming/hospital_quality.R
+++ r_programming/hospital_quality.R
@@ -1,9 +1,7 @@
+data <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings = "Not Available" )
+possible_outcomes <- list("heart attack"=11,"heart failure"=17, "pneumonia"=23)
 
 best <- function(state, outcome) {
-  
-  ## Read outcome data
-  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings = "Not Available" )
-  possible_outcomes <- list("heart attack"=11,"heart failure"=17, "pneumonia"=23)
   
   ## Check that state and outcome are valid
   if (!(state %in% data[,7])) stop("invalid state")
@@ -23,10 +21,7 @@ best <- function(state, outcome) {
 # print(best("NY", "hert attack"))
 
 rankhospital <- function(state, outcome, num = "best") {
-  ## Read outcome data
-  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings = "Not Available" )
-  possible_outcomes <- list("heart attack"=11,"heart failure"=17, "pneumonia"=23)
-  
+
   ## Check that state and outcome are valid
   if (!(state %in% data[,7])) stop("invalid state")
   if (!(outcome %in% names(possible_outcomes))) stop("invalid outcome")
@@ -34,7 +29,7 @@ rankhospital <- function(state, outcome, num = "best") {
   ## Return hospital name in that state with the given rank
   state_data <- data[data$State==state,]
   outcome_col <- possible_outcomes[outcome][[1]]
-  sorted_state_data = state_data[order(as.numeric(state_data[[outcome_col]]), state_data[[2]], na.last=NA),]
+  sorted_state_data <- state_data[order(as.numeric(state_data[[outcome_col]]), state_data[[2]]),]
   
   if (num=="best") num = 1
   if (num=='worst') num = nrow(sorted_state_data)
@@ -42,10 +37,27 @@ rankhospital <- function(state, outcome, num = "best") {
   sorted_state_data[num, 2]
   
 }
-print(rankhospital("TX", "heart failure", 4))
-print(rankhospital("MD", "heart attack", "worst"))
-print(rankhospital("MN", "heart attack", 5000))
+# print(rankhospital("TX", "heart failure", 4))
+# print(rankhospital("MD", "heart attack", "worst"))
+# print(rankhospital("MN", "heart attack", 5000))
 
+rankall <- function(outcome, num = "best") {
+
+  ## For each state, find the hospital of the given rank
+  state <- levels(factor(data[, 7]))
+  hospital <- vector(mode="character")           
+  
+  for (i in seq(state)) {
+    hospital[i] <- rankhospital(state[i], outcome, num)
+  }
+  
+  ## Return a data frame with the hospital names and the
+  data.frame(hospital, state)
+}
+
+print(head(rankall("heart attack", 20), 10))
+print(tail(rankall("pneumonia", "worst"), 3))
+print(tail(rankall("heart failure"), 10))
 
 
 
