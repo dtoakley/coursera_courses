223717bfe7660ffde73465fbc93eca60107beab7
Dan Lewis-Toakley <dan@avaaz.org>
2016-07-03 22:01:33 +0100

    zombie and ozmbie tests added

--- principals_of_computing_2/zombie.py
+++ principals_of_computing_2/zombie.py
@@ -96,21 +96,84 @@ class Apocalypse(poc_grid.Grid):
         Distance at member of entity_list is zero
         Shortest paths avoid obstacles and use four-way distances
         """
-        return
+        grid_height = self.get_grid_height()
+        grid_width = self.get_grid_width()
+
+        self._visited = poc_grid.Grid(grid_height, grid_width)
+        self._distance_field = [[grid_height * grid_width for dummy_col in range(grid_width)]
+                       for dummy_row in range(grid_height)]
+        self._boundary = poc_queue.Queue()
+
+        if entity_type is ZOMBIE:
+            for zombie in self.zombies():
+                    self._boundary.enqueue(zombie)
+        if entity_type is HUMAN:
+            for human in self.humans():
+                self._boundary.enqueue(human)
+
+        for entity in self._boundary:
+            self._visited.set_full(entity[0], entity[1])
+            self._distance_field[entity[0]][entity[1]] = 0
+
+        while len(self._boundary) > 0:
+            cell = self._boundary.dequeue()
+            distance = self._distance_field[cell[0]][cell[1]]
+            neighbours = self._visited.four_neighbors(cell[0], cell[1])
+
+            for neighbour in neighbours:
+                row, col = neighbour[0], neighbour[1]
+                if self._visited.is_empty(row, col) and self.is_empty(row, col):
+                    self._distance_field[row][col] = min(self._distance_field[row][col], distance + 1)
+                    self._visited.set_full(row, col)
+                    self._boundary.enqueue(neighbour)
+        return self._distance_field
 
     def move_humans(self, zombie_distance_field):
         """
         Function that moves humans away from zombies, diagonal moves
         are allowed
         """
-        pass
+        for human in self.humans():
+            human_row, human_col = human[0], human[1]
+            current_dist = zombie_distance_field[human_row][human_col]
+            neighbours = self.eight_neighbors(human_row, human_col)
+
+            best_position = human
+            best_dist = current_dist
+
+            for neighbour in neighbours:
+                neigh_row, neigh_col = neighbour[0], neighbour[1]
+                neigh_dist = zombie_distance_field[neigh_row][neigh_col]
+
+                if neigh_dist > best_dist and self.is_empty(neigh_row, neigh_col):
+                    best_dist = neigh_dist
+                    best_position = neighbour
+
+            self._human_list[self._human_list.index(human)] = best_position
 
     def move_zombies(self, human_distance_field):
         """
         Function that moves zombies towards humans, no diagonal moves
         are allowed
         """
-        pass
+
+        for zombie in self.zombies():
+            zombie_row, zombie_col = zombie[0], zombie[1]
+            current_dist = human_distance_field[zombie_row][zombie_col]
+            neighbours = self.four_neighbors(zombie_row, zombie_col)
+
+            best_position = zombie
+            best_dist = current_dist
+
+            for neighbour in neighbours:
+                neigh_row, neigh_col = neighbour[0], neighbour[1]
+                neigh_dist = human_distance_field[neigh_row][neigh_col]
+
+                if neigh_dist < best_dist and self.is_empty(neigh_row, neigh_col):
+                    best_dist = neigh_dist
+                    best_position = neighbour
+
+            self._zombie_list[self._zombie_list.index(zombie)] = best_position
 
 # Start up gui for simulation - You will need to write some code above
 # before this will work without errors
--- principals_of_computing_2/zombie_test.py
+++ principals_of_computing_2/zombie_test.py
@@ -3,6 +3,15 @@ import pytest, unittest
 from mock import Mock
 from zombie import Apocalypse
 
+# global constants
+EMPTY = 0
+FULL = 1
+FOUR_WAY = 0
+EIGHT_WAY = 1
+OBSTACLE = 5
+HUMAN = 6
+ZOMBIE = 7
+
 class TestClickerState(object):
 
     @pytest.fixture(autouse=True)
@@ -23,7 +32,7 @@ class TestClickerState(object):
     def test_zombies(self):
         # when
         zombies = self.filled_apocalypse.zombies()
-        
+
         # then
         assert next(zombies) == (1, 0)
         assert next(zombies) == (4, 3)
@@ -39,3 +48,35 @@ class TestClickerState(object):
         assert next(humans) == (3, 2)
         with pytest.raises(StopIteration):
             next(humans)
+
+    def test_compute_distance_field(self):
+        # when
+        zombie_distances = self.filled_apocalypse.compute_distance_field(ZOMBIE)
+        human_distances = self.filled_apocalypse.compute_distance_field(HUMAN)
+
+        # then
+        assert zombie_distances == [[1, 20, 3, 4], [0, 1, 2, 3], [1, 2, 3, 2], [2, 3, 2, 1], [3, 4, 20, 0]]
+        assert human_distances == [[5, 20, 3, 2], [4, 3, 2, 1], [3, 2, 1, 0], [2, 1, 0, 1], [3, 2, 20, 2]]
+
+    def test_move_humans(self):
+        # given
+        apocalypse = Apocalypse(3, 3, [], [(2, 2)], [(1, 1)])
+        human_distance_field = [[4, 3, 2], [3, 2, 1], [2, 1, 0]]
+
+        # when
+        apocalypse.move_humans(human_distance_field)
+        humans = apocalypse.humans()
+        # then
+        assert humans.next() == (0, 0)
+
+    def test_move_zombies(self):
+        # given
+        apocalypse = Apocalypse(3, 3, [(1, 1), (1, 2)], [(2, 2)], [(0, 2)])
+        zombie_distance_field = [[2, 1, 0], [3, 9, 9], [4, 5, 6]]
+
+        # when
+        apocalypse.move_zombies(zombie_distance_field)
+        zombies = apocalypse.zombies()
+
+        # then
+        assert zombies.next() == (2, 1)
