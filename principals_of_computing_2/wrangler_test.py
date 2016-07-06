f01cdcba8b8b5fdcfb92983f06ac238d772b4279
Dan Lewis-Toakley <dan@avaaz.org>
2016-07-06 21:52:18 +0100

    merge_sort added

--- principals_of_computing_2/wrangler_test.py
+++ principals_of_computing_2/wrangler_test.py
@@ -42,3 +42,14 @@ class TestWrangler(object):
 
         # then
         assert result == expected
+
+    def test_merge_sort(self):
+        # given
+        to_merge_sort = [1, 9, 9, 5, 7, 7, 4, 5, 2, 3, 4, 8, 6, 10]
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        # when
+        result = merge_sort(to_merge_sort)
+
+        # then
+        assert result == expected
